04657dc731dade9f26aee49983033836a02d75ac
Sovan Sundar Dey <techsovan.github0521@gmail.com>
2025-11-04 20:42:40 +0530

    update page loader

--- frontend/src/components/Page Loader Component/Pageloader.jsx
+++ frontend/src/components/Page Loader Component/Pageloader.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PageLoaderStyle from '../../../public/styles/page loader files/pageloader.module.css';
 
 function Pageloader() {
+
+    useEffect(() => {
+       
+        let thirdPageLoaderBox = document.querySelector(`.${PageLoaderStyle.pageLoaderThirdSection}`);
+        let thirdPageLoaderInnerBox = document.querySelectorAll(`.${PageLoaderStyle.pageLoaderThirdSection} .${PageLoaderStyle.welcomeBox}`);
+        let thirdPageLoaderInnerBoxLength = thirdPageLoaderInnerBox.length;
+
+        thirdPageLoaderBox.style.transform = "translateY(-30vh)";
+
+        for (let i = 0; i < thirdPageLoaderInnerBoxLength; i++){
+
+            thirdPageLoaderInnerBox[i].style.transform = `translateY(-${1000 + (i + 5)}px)`;
+
+        }
+
+        window.addEventListener('load', () => {
+
+            // Use Request Animation Frame
+            requestAnimationFrame(() => {
+
+                thirdPageLoaderBox.style.transform = "translateY(0)";
+                thirdPageLoaderBox.style.transition = "all 0.8s ease";
+
+                for (let i = 0; i < thirdPageLoaderInnerBoxLength; i++){
+
+                    thirdPageLoaderInnerBox[i].style.transform = "translateY(0)";
+                    thirdPageLoaderInnerBox[i].style.transition = `all 2s ease ${i * 0.2}s`;
+                    thirdPageLoaderInnerBox[i].style.backgroundColor = "#ffffffff";
+
+                }
+
+             });
+
+        })
+
+    }, []);
     
     return (
 
