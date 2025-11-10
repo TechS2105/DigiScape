f01b33a356498588beaf86af5c5fdc529a97a027
Sovan Sundar Dey <techsovan.github0521@gmail.com>
2025-11-10 13:41:15 +0530

    update page loader

--- frontend/src/components/Page Loader Component/Pageloader.jsx
+++ frontend/src/components/Page Loader Component/Pageloader.jsx
@@ -9,34 +9,139 @@ function Pageloader() {
         let pageLoaderInnerDiv = document.querySelector(`.${PageLoaderStyle.pageLoaderInnerBox}`); // Page Loader Inner Div
         let welcomeDiv = document.querySelector(`.${PageLoaderStyle.pageLoaderWelcomeDiv}`); // Weolcome Div
         let welcomeDivText = document.querySelector(`.${PageLoaderStyle.pageLoaderWelcomeDiv} p`); // Welcome Div Text
+        let digiscapeBox = document.querySelectorAll(`.${PageLoaderStyle.pageLoaderDigiscapeDiv} .${PageLoaderStyle.digiscapeBox}`);
+        let digiscapeBoxText = document.querySelectorAll(`.${PageLoaderStyle.digiscapeBox} span`);
 
+        // Sliding up the page loader main div
         pageLoaderInnerDiv.style.transform = "translateY(-100vh)";
+
+        // Zooming out welcome div 
+        welcomeDiv.style.transform = "scaleY(0) translate(-50%, -50%)";
+        welcomeDiv.style.opacity = "0";
+        welcomeDiv.style.filter = "blur(20px)";
+
+        // Zooming out welcome div text
+        welcomeDivText.style.transform = "scaleX(0)";
+        welcomeDivText.style.opacity = "0";
+        welcomeDivText.style.filter = "blur(20px)";
+
+        // Selecting Digiscape div odd and even inner boxes and sliding them up and down words
+        digiscapeBox.forEach((item, id) => {
+
+            if (id % 2 === 1) {
+                
+                item.style.transform = `translateY(-${1000 + (id + 5)}px)`;
+                item.style.opacity = "0";
+
+            } else {
+                
+                item.style.transform = `translateY(${1000 + (id + 5)}px)`;
+                item.style.opacity = "0";
+
+            }
+
+        });
+
+        // Selecting Digiscape div odd and even inner box text's
+        digiscapeBoxText.forEach((txt, id) => {
+
+            if (id % 2 === 1) {
+                
+                txt.style.transform = `translateX(${id + 200}px)`;
+                txt.style.opacity = "0";
+                txt.style.filter = "blur(20px)";
+
+            } else {
+                
+                txt.style.transform = `translateX(-${id + 200}px)`;
+                txt.style.opacity = "0";
+                txt.style.filter = "blur(20px)";
+
+            }
+
+        })
         
         window.addEventListener("load", () => {
 
             requestAnimationFrame(() => {
 
+                // Keep center the page loader main div 
                 pageLoaderInnerDiv.style.transform = "translateY(0)";
                 pageLoaderInnerDiv.style.transition = "all 0.8s ease";
 
+                // Zoom in welcome div
+                welcomeDiv.style.transform = "scaleY(1) translate(-50%, -50%)";
+                welcomeDiv.style.opacity = "1";
+                welcomeDiv.style.filter = "blur(0px)"
+                welcomeDiv.style.transition = "all 0.8s ease 0.5s";
+
+                // Zoom in welcome div text
+                welcomeDivText.style.transform = "scaleX(1)";
+                welcomeDivText.style.opacity = "1";
+                welcomeDivText.style.filter = "blur(0px)";
+                welcomeDivText.style.transition = "all 0.8s ease 1s";
+
+                // Selecting digiscape div odd and even inner boxes and keep them center align
+                digiscapeBox.forEach((item, id) => {
+
+                    if (id % 2 === 1) {
+                        
+                        item.style.transform = `translateY(0)`;
+                        item.style.transition = `all 3s ease ${id * 0.2}s`;
+                        item.style.opacity = "1";
+
+                    } else {
+                        
+                        item.style.transform = `translateY(0)`;
+                        item.style.opacity = "1";
+                        item.style.filter = "blur(0px)";
+                        item.style.transition = `all 2s ease ${id * 0.3}s`;
+
+                    }
+
+                });
+
+                // Selecting Digiscape div odd and even inner boxes text and keep them center align
+                digiscapeBoxText.forEach((txt, id) => {
+
+                    if (id % 2 === 1) {
+                        
+                        txt.style.transform = `translateX(0)`;
+                        txt.style.opacity = "1";
+                        txt.style.filter = "blur(0px)";
+                        txt.style.transition = `all 2s ease ${id * 0.3}s`;
+
+                    } else {
+                        
+                        txt.style.transform = `translateX(0)`;
+                        txt.style.opacity = "1";
+                        txt.style.filter = "blur(0px)";
+                        txt.style.transition = `all 2s ease ${id * 0.4}s`;
+
+                    }
+
+                })
+
             });
 
-            /** Use setTimeOut for end the animation */
-            setTimeout(() => {
+            /** Use setTimeOut */
+            // setTimeout(() => {
 
-                pageLoaderInnerDiv.style.transform = "translateY(100vh)";
+                // Sliding down the page loader inner div
+                // pageLoaderInnerDiv.style.transform = "translateY(100vh)";
 
 
-            }, 5000);
+            // }, 5000);
 
-            setTimeout(() => {
+            // setTimeout(() => {
 
-                pageLoaderMainDiv.style.opacity = "0";
-                pageLoaderMainDiv.style.transform = "translateY(-100vh)";
-                pageLoaderMainDiv.style.transition = "all 0.8s ease";
+                // Sliding up the the page loader main div
+                // pageLoaderMainDiv.style.opacity = "0";
+                // pageLoaderMainDiv.style.transform = "translateY(-100vh)";
+                // pageLoaderMainDiv.style.transition = "all 0.8s ease";
                 
 
-            }, 5500);
+            // }, 5800);
 
         });
         
